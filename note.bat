67192a7c957da7a63364ffed9d082917d03bd890
GaryNLOL <dev.garynlol@gmail.com>
2022-01-12 18:11:38 -0500

    Reorganized folder structure and build system.

--- note.bat
+++
@@ -1,6 +0,0 @@
-goto start
-# You have to set the following environment variables for the build to work on Chrome and Firefox (Not tested in other browsers).
-:start
-echo Setting variables
-set INLINE_RUNTIME_CHUNK=false
-set GENERATE_SOURCEMAP=false
