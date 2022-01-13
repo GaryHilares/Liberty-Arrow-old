54116efc1c761f728b8f9a522c607ef590a15588
GaryNLOL <dev.garynlol@gmail.com>
2022-01-13 10:44:27 -0500

    Improved navbar style

--- src/settings_site/src/Components/NavBar.js
+++ src/settings_site/src/Components/NavBar.js
@@ -4,9 +4,12 @@ import NavBarStyles from './NavBar.module.css';
 
 export function NavBar() {
   return (
-    <div className={NavBarStyles.navbar}>
-      <Link to='/home' className={NavBarStyles.navbar__button}>Home</Link>
-      <Link to='/pages' className={NavBarStyles.navbar__button}>Pages</Link>
-      <Link to='/settings' className={NavBarStyles.navbar__button}>Settings</Link>
-    </div>);
+    <nav className={NavBarStyles.navbar}>
+      <img class={NavBarStyles.navbar__logo} alt="logo" src="static/images/wireframe.png" width={"240px"} height={"80px"} />
+      <ul className={NavBarStyles.navbar__unordered_list}>
+        <li class={NavBarStyles.navbar__unordered_list__list_item}><Link to='/home' className={NavBarStyles.navbar__button}>Home</Link></li>
+        <li class={NavBarStyles.navbar__unordered_list__list_item}><Link to='/pages' className={NavBarStyles.navbar__button}>Pages</Link></li>
+        <li class={NavBarStyles.navbar__unordered_list__list_item}><Link to='/settings' className={NavBarStyles.navbar__button}>Settings</Link></li>
+      </ul>
+    </nav>);
 }
