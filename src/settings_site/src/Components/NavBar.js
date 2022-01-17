c5374f137f4be93a3875955577f3c4dc04be63af
GaryNLOL <dev.garynlol@gmail.com>
2022-01-17 14:15:34 -0500

    Added logo

--- src/settings_site/src/Components/NavBar.js
+++ src/settings_site/src/Components/NavBar.js
@@ -5,7 +5,7 @@ import NavBarStyles from './NavBar.module.css';
 export function NavBar() {
   return (
     <nav className={NavBarStyles.navbar}>
-      <img class={NavBarStyles.navbar__logo} alt="logo" src="static/images/wireframe.png" width={"240px"} height={"80px"} />
+      <img class={NavBarStyles.navbar__logo} alt="logo" src="static/images/Liberty_Arrow_logo.png" width={"213px"} height={"76px"} />
       <ul className={NavBarStyles.navbar__unordered_list}>
         <li class={NavBarStyles.navbar__unordered_list__list_item}><Link to='/home' className={NavBarStyles.navbar__button}>Home</Link></li>
         <li class={NavBarStyles.navbar__unordered_list__list_item}><Link to='/pages' className={NavBarStyles.navbar__button}>Pages</Link></li>
